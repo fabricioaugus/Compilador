3940655eb0ac2d6a736908c518c28a2e1d1b3026
fabricioaugus <fabricio10152@gmail.com>
2023-04-09 21:59:52 -0300

    caminhoTirado

--- AnalisadorLexico/AnalisadorLexico/src/analizadorLexico/main.java
+++ AnalisadorLexico/AnalisadorLexico/src/analizadorLexico/main.java
@@ -9,7 +9,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//String lexerFilePath = "CaminhoParaClasseGerarOLexerDoAnalizador";
-                String lexerFilePath ="C:/Users/fabri/Desktop/com/Compilador/AnalisadorLexico/AnalisadorLexico/src/analizadorLexico/Lexer.flex";
+                String lexerFilePath ="CaminhoParaGerarClassQueVaiSerUsadoAnalizador";
 		lexerGenerate(lexerFilePath);
 		
 	}
